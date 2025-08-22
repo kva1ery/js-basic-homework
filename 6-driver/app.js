c6494932634f85ba0a92f961e3a3ee87be5c754e
Valeriy Kokorev <k.va1ery@yandex.ru>
2025-08-22 19:04:28 +0700

    fixed can drive validator

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -2,4 +2,4 @@ const hasLisence = true;
 const age = 18;
 const isDrunk = false;
 
-const canDrive = age >= 18 && hasLisence && !isDrunk;
+const canDrive = age >= 18 && hasLisence && !isDrunk ? 'может' : 'не может';
