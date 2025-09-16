1d42440e4a7831c3fbf3a3a25a4fe068847b11a6
Valeriy Kokorev <k.va1ery@yandex.ru>
2025-09-16 16:24:14 +0700

    fixed password crypter

--- 8-crypto/app.js
+++ 8-crypto/app.js
@@ -1,20 +1,28 @@
 function crypto(value) {
-    if (!value || value.length < 2) {
+    if (!value) {
         return;
     }
     
+    if (value.length == 1) {
+        return value;
+    }
+
     const symbols = value.split('');
+    if (value.length === 2) {
+        return symbols.reverse().join('');
+    }
+
     const middle = Math.floor(symbols.length / 2);
     const firstPie = symbols.splice(0, middle);
 
-    return firstPie.reverse().join("") + symbols.reverse().join("");
+    return firstPie.reverse().join('') + symbols.reverse().join('');
 }
 
 function check(encryptedValue, value) {
     return crypto(encryptedValue) === value;
 }
 
-const password = 'password1';
+const password = 'password';
 const encryptedPassword = crypto(password);
 console.log(encryptedPassword);
 console.log(check(encryptedPassword, password));
